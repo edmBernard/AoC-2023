ac581ad18e23a3c2f72d970c756d4b7176d7cc7c
Erwan Bernard <ebernard@dxo.com>
2023-12-01 08:51:30 +0100

    day01: add rust and zig solution

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::time::Instant;
 
 type Result<T> = ::std::result::Result<T, Box<dyn (::std::error::Error)>>;
@@ -13,31 +14,58 @@ fn main() -> Result<()> {
 
   let mut part1: u64 = 0;
   let mut part2: u64 = 0;
-  let nrun = 10000;
+  let nrun = 100;
 
   for _ in 0..nrun {
-    let mut input_puzzle = Vec::new();
-    let mut one_elf = 0;
+    let mut digits1: Vec<i32> = Vec::new();
+    let mut digits2: Vec<u64> = Vec::new();
     for line in std::fs::read_to_string(filename)?.lines() {
       let line_str = line;
-      if line_str.is_empty() {
-        input_puzzle.push(one_elf);
-        one_elf = 0;
-        continue;
+      // part1
+      let parsed_line: Vec<i32> = line_str
+        .as_bytes()
+        .iter()
+        .map(|c| *c as i32 - '0' as i32)
+        .filter(|d| *d >= 0 && *d < 10i32)
+        .collect::<Vec<_>>();
+      let parsed_len = parsed_line.len();
+      if parsed_len > 0 {
+        digits1.push(parsed_line[0] * 10 + parsed_line[parsed_len - 1]);
       }
-      let value = line_str.parse::<u64>()?;
-      one_elf += value;
+      // part2
+      let mut parsed_line: Vec<u64> = vec![];
+      let line_len = line_str.len();
+      // We use this method because word can overlap like "nineight"
+      for i in 0..line_len {
+        let slice = &line_str[i..];
+        if let Some(d) = slice.chars().nth(0).unwrap().to_digit(10) {
+          println!("slice {}", slice);
+          parsed_line.push(d as u64);
+        } else if slice.starts_with("one") {
+            parsed_line.push(1);
+        } else if slice.starts_with("two") {
+            parsed_line.push(2);
+        } else if slice.starts_with("three") {
+            parsed_line.push(3);
+        } else if slice.starts_with("four") {
+            parsed_line.push(4);
+        } else if slice.starts_with("five") {
+            parsed_line.push(5);
+        } else if slice.starts_with("six") {
+            parsed_line.push(6);
+        } else if slice.starts_with("seven") {
+            parsed_line.push(7);
+        } else if slice.starts_with("eight") {
+            parsed_line.push(8);
+        } else if slice.starts_with("nine") {
+            parsed_line.push(9);
+        }
+      }
+      let parsed_len = parsed_line.len();
+      digits2.push(parsed_line[0] * 10 + parsed_line[parsed_len - 1]);
     }
-    // Push last elf inventory if the puzzle don't end with new line
-    if one_elf != 0 {
-      input_puzzle.push(one_elf);
-    }
-
-    input_puzzle.sort();
-    input_puzzle.reverse();
-
-    part1 = input_puzzle[0];
-    part2 = input_puzzle[0..3].iter().sum();
+    part1 = digits1.iter().sum::<i32>() as u64;
+    part2 = digits2.iter().sum();
   }
   let duration = now.elapsed().as_micros();
   println!(
--- src/day01.zig
+++ src/day01.zig
@@ -32,7 +32,7 @@ pub fn main() !void {
     var tic = std.time.microTimestamp();
     var part1: u64 = 0;
     var part2: u64 = 0;
-    const nrun = 10000;
+    const nrun = 100;
     for (0..nrun) |_| {
         var file = try std.fs.cwd().openFile(filename.?, .{ .mode = .read_only });
         defer file.close();
@@ -43,22 +43,64 @@ pub fn main() !void {
         var buf: [1024]u8 = undefined;
 
         var acc: u64 = 0;
-        var input_puzzle = std.ArrayList(u64).init(allocator);
-        defer input_puzzle.deinit();
+        _ = acc;
+        var digits1 = std.ArrayList(u64).init(allocator);
+        defer digits1.deinit();
+        var digits2 = std.ArrayList(u64).init(allocator);
+        defer digits2.deinit();
 
         while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-            if (line.len == 0) {
-                try input_puzzle.append(acc);
-                acc = 0;
-                continue;
+            // part1
+            {
+                var parsedLine = std.ArrayList(u64).init(allocator);
+                defer parsedLine.deinit();
+                for (line) |c| {
+                    var digit = c - '0';
+                    if (digit >= 0 and digit < 10) {
+                        try parsedLine.append(digit);
+                    }
+                }
+                if (parsedLine.items.len > 0) {
+                    try digits1.append(parsedLine.items[0] * 10 + parsedLine.items[parsedLine.items.len - 1]);
+                }
             }
-
-            const integer = try std.fmt.parseUnsigned(u64, line, 10);
-            acc += integer;
+            // part2
+            {
+                var parsedLine = std.ArrayList(u64).init(allocator);
+                defer parsedLine.deinit();
+                // We use this method because word can overlap like "nineight"
+                for (0..line.len) |i| {
+                    var slice = line[i..];
+                    var digit = slice[0] - '0';
+                    if (digit >= 0 and digit < 10) {
+                        try parsedLine.append(digit);
+                    } else if (std.mem.eql(u8, slice[0..3], "one")) {
+                        try parsedLine.append(1);
+                    } else if (std.mem.eql(u8, slice[0..3], "two")) {
+                        try parsedLine.append(2);
+                    } else if (std.mem.eql(u8, slice[0..5], "three")) {
+                        try parsedLine.append(3);
+                    } else if (std.mem.eql(u8, slice[0..4], "four")) {
+                        try parsedLine.append(4);
+                    } else if (std.mem.eql(u8, slice[0..4], "five")) {
+                        try parsedLine.append(5);
+                    } else if (std.mem.eql(u8, slice[0..3], "six")) {
+                        try parsedLine.append(6);
+                    } else if (std.mem.eql(u8, slice[0..5], "seven")) {
+                        try parsedLine.append(7);
+                    } else if (std.mem.eql(u8, slice[0..5], "eight")) {
+                        try parsedLine.append(8);
+                    } else if (std.mem.eql(u8, slice[0..4], "nine")) {
+                        try parsedLine.append(9);
+                    }
+                }
+                if (parsedLine.items.len > 0) {
+                    try digits2.append(parsedLine.items[0] * 10 + parsedLine.items[parsedLine.items.len - 1]);
+                }
+            }
         }
-        std.sort.pdq(u64, input_puzzle.items, {}, std.sort.desc(u64));
-        part1 = input_puzzle.items[0];
-        part2 = sum(input_puzzle.items[0..3]);
+        part1 = sum(digits1.items);
+        part2 = sum(digits2.items);
     }
     var tac: i64 = std.time.microTimestamp() - tic;
     std.log.info("Zig  day01 in {d:>20.2} us : part1={:<10} part2={:<10}", .{ @as(f32, @floatFromInt(tac)) / @as(f32, nrun), part1, part2 });
