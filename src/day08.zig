1a3d8d733d5f34ce3c7ae219fdae5c593f75c7df
Erwan Bernard <ebernard@dxo.com>
2023-12-08 08:31:48 +0100

    day08: zig part1

--- src/day08.zig
+++ src/day08.zig
@@ -24,82 +24,96 @@ pub fn main() !void {
     var tic = std.time.microTimestamp();
     var part1: u64 = 0;
     var part2: u64 = 0;
-    const nrun = 10000;
+    const nrun = 1;
     for (0..nrun) |_| {
         var file = try std.fs.cwd().openFile(filename.?, .{ .mode = .read_only });
         defer file.close();
 
         var read_buf = try file.readToEndAlloc(allocator, 1024 * 1024);
         defer allocator.free(read_buf);
-        var it = std.mem.splitAny(u8, read_buf, "\n");
+        var line_it = std.mem.splitAny(u8, read_buf, "\n");
 
-        var acc_part1: u64 = 0;
-        var acc_part2: u64 = 0;
-
-        while (it.next()) |line| {
-            // part1
-            {
-                var firstDigit: u32 = 0;
-                var lastDigit: u32 = 0;
-                forward: for (0..line.len) |idx| {
-                    var digit = line[idx] - '0';
-                    if (digit >= 0 and digit < 10) {
-                        firstDigit = digit;
-                        break :forward;
-                    }
+        // Parse instruction
+        const InstructionTag = enum { left, right };
+        var instructions = std.ArrayList(InstructionTag).init(allocator);
+        defer instructions.deinit();
+        {
+            var first_line = line_it.next().?;
+            try instructions.ensureUnusedCapacity(first_line.len);
+            for (first_line) |c| {
+                switch (c) {
+                    'R' => instructions.appendAssumeCapacity(InstructionTag.right),
+                    'L' => instructions.appendAssumeCapacity(InstructionTag.left),
+                    else => unreachable,
                 }
-                backward: for (0..line.len) |idx| {
-                    var digit = line[line.len - idx - 1] - '0';
-                    if (digit >= 0 and digit < 10) {
-                        lastDigit = digit;
-                        break :backward;
-                    }
-                }
-                acc_part1 += firstDigit * 10 + lastDigit;
-            }
-            // part2
-            {
-                var digits_string = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-                var firstDigit: u64 = 0;
-                var lastDigit: u64 = 0;
-                // We use this method because word can overlap like "nineight"
-                forward: for (0..line.len) |idx| {
-                    var digit = line[idx] - '0';
-                    if (digit >= 0 and digit < 10) {
-                        firstDigit = digit;
-                        break :forward;
-                    } else {
-                        var slice = line[idx..];
-                        for (digits_string, 1..) |str, value| {
-                            if (std.mem.startsWith(u8, slice, str)) {
-                                firstDigit = value;
-                                break :forward;
-                            }
-                        }
-                    }
-                }
-                backward: for (0..line.len) |idx| {
-                    var ridx = line.len - idx - 1;
-                    var digit = line[ridx] - '0';
-                    if (digit >= 0 and digit < 10) {
-                        lastDigit = digit;
-                        break :backward;
-                    } else {
-                        var slice = line[ridx..];
-                        for (digits_string, 1..) |str, value| {
-                            if (std.mem.startsWith(u8, slice, str)) {
-                                lastDigit = value;
-                                break :backward;
-                            }
-                        }
-                    }
-                }
-                acc_part2 += firstDigit * 10 + lastDigit;
             }
         }
-        part1 = acc_part1;
-        part2 = acc_part2;
+
+        // parse node
+        var current_node_part2 = std.ArrayList([]const u8).init(allocator);
+        var graph = std.StringHashMap([2][]const u8).init(allocator);
+        defer graph.deinit();
+
+        while (line_it.next()) |line| {
+            if (line.len == 0)
+                continue;
+
+            var it = std.mem.tokenizeAny(u8, line, " =(,)");
+            var node_name = it.next().?;
+
+            var left_name = it.next().?;
+            var right_name = it.next().?;
+            try graph.put(node_name, .{ left_name, right_name });
+
+            if (node_name[2] == 'A')
+                try current_node_part2.append(node_name);
+        }
+
+        // part 1
+        var step_count_part1: u64 = 0;
+        {
+            var instruction_index: usize = 0;
+            var current_node: []const u8 = "AAA";
+            while (!std.mem.eql(u8, current_node, "ZZZ")) {
+                current_node = switch (instructions.items[instruction_index]) {
+                    InstructionTag.left => graph.get(current_node).?[0],
+                    InstructionTag.right => graph.get(current_node).?[1],
+                };
+                instruction_index = if (instruction_index == instructions.items.len - 1) 0 else instruction_index + 1;
+                step_count_part1 += 1;
+            }
+        }
+        // part 2
+        // Don't seem to work for the moment.
+        var step_count_part2: u64 = 0;
+        {
+            var instruction_index: usize = 0;
+            var all_node_are_ending = false;
+            while (!all_node_are_ending) {
+                // std.debug.print("Nodes : ", .{});
+                // for (current_node_part2.items) |node| {
+                //     std.debug.print("{s} ", .{node});
+                // }
+                // std.debug.print("\n", .{});
+
+                all_node_are_ending = true;
+                for (current_node_part2.items) |*node| {
+                    // std.debug.print("current node {s}\n", .{node.*});
+                    node.* = switch (instructions.items[instruction_index]) {
+                        InstructionTag.left => graph.get(node.*).?[0],
+                        InstructionTag.right => graph.get(node.*).?[1],
+                    };
+                    all_node_are_ending = all_node_are_ending and node.*[2] == 'Z';
+                }
+                instruction_index = if (instruction_index == instructions.items.len - 1) 0 else instruction_index + 1;
+                step_count_part2 += 1;
+                // if (@mod(step_count_part2, 100000) == 0)
+                //     std.debug.print("step = {d}\n", .{step_count_part2});
+            }
+        }
+        part1 = step_count_part1;
+        part2 = step_count_part2;
     }
     var tac: i64 = std.time.microTimestamp() - tic;
-    std.log.info("Zig  day01 in {d:>20.2} us : part1={:<10} part2={:<10}", .{ @as(f32, @floatFromInt(tac)) / @as(f32, nrun), part1, part2 });
+    std.log.info("Zig  day08 in {d:>20.2} us : part1={:<10} part2={:<10}", .{ @as(f32, @floatFromInt(tac)) / @as(f32, nrun), part1, part2 });
 }
